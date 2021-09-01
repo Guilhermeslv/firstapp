26633300029aa3dd69af00a2c1670d439353aa41
Guilherme Silva <2016infor27@gmail.com>
2021-09-01 20:56:02 -0300

    Alteracoes finais

--- index.js
+++ index.js
@@ -2,19 +2,46 @@ const express = require("express");
 const app = express();
 const routes = require("./routes");
 
-const expressLayouts = require("express-ejs-layouts")
+const expressLayouts = require("express-ejs-layouts");
+const { urlencoded } = require("express");
 
-const port=3030;
-const address="localhost";
+const port = 3000;
+const address = "localhost";
+
+const utils = require("./utils");
+
+const faker = require("faker"); 
+
+let toggleBol=true;
+
+
+global.users =[];
+
+for (let cont=0;cont<20;cont++){
+    users.push({
+        name:faker.name.findName(),
+        email:faker.internet.email(),
+        address:faker.address.streetAddress(),
+        age:utils.getRandomByInterval(15,50,true),
+        heigth:utils.getRandomByInterval(1.50,1.70,false).toFixed(2),
+        vote:toggleBol
+    });
+        
+    toggleBol=!toggleBol;
+}
+
+
 
 app.set('view engine','ejs');
 app.use(expressLayouts);
 
-app.use('/', routes);
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
 
-//Criando um servidor simples com o Node.js e o Express
+app.use('/',routes);
+
 const server = app.listen(port,address,()=>{
- let host = server.address().address;
- let port = server.address().port;
- console.log(`Servidor executando no endereço ${host} e porta ${port}`);
+    let host = server.address().address;
+    let port = server.address().port;
+    console.log(`> Server rodando em http://${host}:${port}`);
 });
--- routes.js
+++ routes.js
@@ -6,23 +6,106 @@ router.use(express.static('public'));
 router.get('/',(req,res)=>{
     res.render('pages/home');
 });
-router.get('/about',(req,res)=>{
+
+router.get('/about',(req,res)=>{ 
     res.render('pages/about');
 });
-router.get('/cadastro',(req,res)=>{
-    let users = [
-        {name: "Cecilia C. Nogueira", addres:"Rua tal, 90",email:"cecilia@gmail.com",age:19,height:1.67,vote:true},
-        {name: "Luis Guilherme da Silva", addres:"Rua tal, 90",email:"guilherme@gmail.com",age:19,height:1.92,vote:true},
-        {name: "Maria Eduarda da Silva", addres:"Rua acula, 60",email:"maria@gmail.com",age:17,height:1.67,vote:true}
 
+router.get('/cadastro',(req,res)=>{ 
+    res.render('pages/cadastro',{users: users}); 
+});
 
-    ];
-    res.render('pages/cadastro',{users});
+router.post('/cadastro/remove',(req,res)=>{
+    let name = req.body.name;
+
+    if(users.length==0) {
+        console.log("Erro: Não há elemento a ser removido!");
+        return res.status(500).son({
+            status:'error',
+            error:`Removed element: ${name}`
+        })
+    } else{
+        for(let cont=0; cont<users.length; cont++){
+            if(users[cont].name==name){
+                users.splice(cont, 1)
+                console.log(name, "removido!" )
+                return res.status(200).json({
+                    status:'success',
+                    data:users
+                })
+            }
+        }
+    }
+
+    users.splice(name, 1);
 });
-router.get('/cadastro/remove/:id',(req,res)=>{
-    res.send("Remoção realizada com sucesso!");
+
+router.post('/cadastro/update',(req,res)=>{
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const isValid = true;
+    
+    
+    //validações
+    if (!emailRegex.test(req.body.email)) {
+        isValid == false;
+    }
+    if (req.body.vote.toLowerCase() != "yes" || req.body.vote.toLowerCase() != "no") {
+        isValid == false;
+    }
+    if (typeof req.body.age != Number) {
+        isValid == false;
+    }
+    
+    
+    if (isValid) {
+        users[req.body.id].name=req.body.name;
+        users[req.body.id].email=req.body.email;
+        users[req.body.id].address=req.body.address;
+        users[req.body.id].age=req.body.age;
+        users[req.body.id].heigth=req.body.heigth;
+        users[req.body.id].vote=req.body.vote;
+    
+        res.sendStatus(200);
+        console.log("Dados recebidos: ",req.body);
+    }
+
+    res.sendStatus(400);
 });
-router.get('/cadastro/update/:id',(req,res)=>{
-    res.send("Atualização realizada com sucesso!");
+
+router.get('/cadastro/list',(req,res)=>{
+    console.log("Lista: ",users);
+    res.send(JSON.stringify(users));
 });
+
+router.post('/cadastro/addUser',(req,res)=>{
+    let user = {name:"",email:"",address:"",heigth:"",age:"",vote:""};
+
+    user.name = req.body.name;
+    user.email = req.body.email;
+    user.address = req.body.address;
+    user.heigth = req.body.heigth;
+    user.age = req.body.age;
+    user.vote = req.body.vote;
+
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const addressRegex = /([a-zA-Z])\w+,*([a-zA-Z])\w+/
+
+    if (!emailRegex.test(user.email)) {
+        res.sendStatus(400);
+        return
+    }
+
+    if (!addressRegex.test(user.address)) {
+        res.sendStatus(400);
+        return
+    }
+
+    user.heigth = Number(user.heigth).toFixed(2);
+    user.heigth += "m";
+
+    users.push(user);
+
+    res.sendStatus(200)
+});
+
 module.exports = router;
